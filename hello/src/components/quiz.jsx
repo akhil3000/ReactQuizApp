d6c6f874a06e1d0498fd6c001c2843e20f699886
AkhilPolisetty <akhilpolisetty456@gmail.com>
2025-08-20 18:49:49 +0530

    QuizApp layout has been added

--- hello/src/components/quiz.jsx
+++ hello/src/components/quiz.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 function Quiz(){
   const questionBank=[
   {
@@ -13,18 +14,68 @@ function Quiz(){
   },
   {
     question:"Who is CEO of Google",
-    options:["Sundar","Bezos","Musk","Jobs"]
+    options:["Sundar","Bezos","Musk","Jobs"],
+    answer:"Sundar"
 
   }
 
 
   ];
 
+    
+
+   const intialAnswers=["null","null","null"];
+   const[userAnswers,setUserAnswers]=useState(intialAnswers);
+   const[currentQuestion,setCurrentQuestion]=useState(0); 
+   const selectedAnswer=userAnswers[currentQuestion];
+
+   function handleSelectOption(option){
+       const newUserAnswers=[...userAnswers];
+       newUserAnswers[currentQuestion]=option;
+       setUserAnswers(newUserAnswers);
+   }
+
+    function goToNext(){
+       if(currentQuestion<2){ 
+       setCurrentQuestion(currentQuestion+1);
+       }  
+    }
+
+    function goToPrevious()
+    {
+        if(currentQuestion>0)
+        {    
+        setCurrentQuestion(currentQuestion-1);
+        }
+    }
 
 
 
 
+    return( 
+    
+    <div>
+        <h2>Question{currentQuestion}</h2>
+        <p className="question">{questionBank[currentQuestion].question}</p>
+        {questionBank[currentQuestion].options.map((option)=>(
+        <button className={"option" +(selectedAnswer===option?"selected":"") }
+        onClick={()=>handleSelectOption(option)}>
+        
+            {option}</button>
+        ))}
+     
+        <div className="nav-buttons">
+          <button onClick={goToPrevious} disabled={currentQuestion===0} >
+          {" "}
+          Previous</button>
+          <button onClick={goToNext} disabled={!selectedAnswer}>
+          {currentQuestion===questionBank.length-1?"Finish Quiz":"Next"}
+          </button>
 
-    return <div>quiz</div>
+        </div>       
+
+    </div>
+    );
 }
 export default Quiz;
+
